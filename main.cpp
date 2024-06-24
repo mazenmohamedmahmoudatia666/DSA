c43703471c444886a9a9f8613a520c74677c4ef5
mazen mohamed <email>
2024-06-24 19:59:43 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -19,22 +19,27 @@ void fastio()
 // ---------------------------------------------------------------------------
 void solve()
 {
-  int n;
-  cin >> n;
-   vector < int > a(n) , b(n)   ;
-   for(int i =0; i < n; i++)
-    cin >> a[i];
-     for(int i =0; i < n; i++)
-    cin >> b[i];
-    vector <int > c(2*n);
-    for(int i = 0; i < 2*n; i++){
-      if(a.size || b.size()){
-        c.insert(a[i]);
-        c.insert(b[i]);
-      }
-
-      
+  #include <vector>
+#include <algorithm>
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) { 
+        // Sort the nums vector in non-ascending order
+        sort(nums.rbegin(), nums.rend());
+        
+        // Iterate over the nums vector
+        for(int i = 0; i < (int)nums.size() - 2; i++) {
+            // Check if the current triplet forms a valid triangle
+            if(nums[i] < nums[i+1] + nums[i+2]) {
+                // If it does, return the perimeter of the triangle
+                return nums[i] + nums[i+1] + nums[i+2];
+            }
+        }
+        // If no valid triangle is found, return 0
+        return 0;
     }
+};
+
      
 
    
