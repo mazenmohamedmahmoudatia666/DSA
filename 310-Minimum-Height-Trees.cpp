0d37f6b1fd774e002cdeb9c11515045785f6d773
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-10-19 13:05:41 +0300

    [310] [Time Beats: 100.00%] [Memory Beats: 91.94%] - LeetPush

--- 310-Minimum-Height-Trees.cpp
+++ 310-Minimum-Height-Trees.cpp
@@ -53,3 +53,114 @@ public:
         return result;
     }
 };
+
+
+/*
+// https://leetcode.com/problems/minimum-height-trees/
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<unordered_map>
+using namespace std;
+
+/*
+
+We can develop an idea that is inspired from BFS and implemented like a topological sort!
+
+The idea is shrinking (deleting) the tree leaves LEVEL-by-LEVEL
+\tUndirected tree node will be leave if in-degree = 1
+
+Imagine a chain: 1-2-3-4-5-6-7
+\tNodes 1 and 7 are the current chains. Remove them
+\t2-3-4-5-6
+\tNext leaves: 2 and 6. Remove (level 2)
+\t3-4-5
+\tNext leaves: 3 and 5. Remove (level 3)
+\t4
+
+\tThis is for odd length
+
+\tFor even length: 1-2-3-4-5-6-7-8
+\tStop when there are only 2 nodes: 4-5
+
+The same approach for a tree. The proof is intuitive.
+\tLeave nodes will make the tree height very high
+\tThe centroids somewhere in the tree are affected by these leaves
+\tif a centroid at height 7, after we remove ALL leaves at current level,
+\t\tthe this centroid height is decreasing by one
+
+\tSee editorial figures
+
+Implementation:
+\tLike topological sort but
+\t\tgraph is undirected
+\t\tthe focus on in-degree = 1
+\t\tStop when there are nodes <= 2
+
+ */
+/*
+typedef vector<vector<int>> GRAPH;
+
+void add_undirected_edge(GRAPH &graph, int from, int to) {
+\tgraph[from].push_back(to);
+\tgraph[to].push_back(from);
+}
+
+vector<int> tree_centroids(const GRAPH &adjList) {\t// O(V)
+\tint sz = adjList.size();
+
+\tif(sz == 1)\t\t// special case
+\t\treturn {0};
+
+\tvector<int> indegree(sz, 0);
+\tfor (int i = 0; i < sz; ++i)
+\t\tfor (int j : adjList[i])
+\t\t\tindegree[j]++;
+
+\tqueue<int> ready;
+\tfor (int i = 0; i < sz; ++i)
+\t\tif (indegree[i] == 1)\t// leaves
+\t\t\tready.push(i);
+
+\t// keep shrinking leaves: level by level
+\twhile (sz > 2) {
+\t\tint level_sz = ready.size();\t// how many leaves NOW
+\t\tsz -= level_sz;
+\t\twhile (level_sz--) {\t// level by level like BFS
+\t\t\tint i = ready.front();
+\t\t\tready.pop();
+
+\t\t\tfor (int j : adjList[i])
+\t\t\t\tif (--indegree[j] == 1)
+\t\t\t\t\tready.push(j);
+\t\t}
+\t}
+\tvector<int> res;
+\twhile (!ready.empty()) {
+\t\tres.push_back(ready.front());
+\t\tready.pop();
+\t}
+\treturn res;
+}
+
+class Solution {
+public:
+\tvector<int> findMinHeightTrees(int n, vector<vector<int>> &edges) {
+\t\tGRAPH graph(edges.size() + 1);
+
+\t\tfor (auto &edge : edges)
+\t\t\tadd_undirected_edge(graph, edge[0], edge[1]);
+
+\t\treturn tree_centroids(graph);
+\t}
+};
+
+int main() {
+
+\treturn 0;
+}
+
+
+
+*/
