bfc977d17e6ecf5c54d6dec95d3407fa6f8f8638
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-10-22 12:18:11 +0300

    [1691] [Time Beats: 57.20%] [Memory Beats: 51.44%] - LeetPush

--- 1691-Maximum-Height-by-Stacking-Cuboids-.cpp
+++ 1691-Maximum-Height-by-Stacking-Cuboids-.cpp
@@ -1,65 +1,48 @@
-// https://leetcode.com/problems/maximum-height-by-stacking-cuboids/
-
-/*
-
-
- The problem also can be solved using topological sort
- https://leetcode.com/problems/maximum-height-by-stacking-cuboids/discuss/970288/PythonC%2B%2B-O(n2)-Topological-Sort
- */
-
 #include <iostream>
 #include <vector>
 #include <cstring>
 #include <algorithm>
+
 using namespace std;
-
-vector<vector<int>> cuboidsg;
-const int MAX = 100 + 1;
-int memory[MAX][MAX];
-
-bool less_eq(int i, int j) {
-	// you don't need to compare on [0] as sorting does
-	return cuboidsg[i][1] <= cuboidsg[j][1] && cuboidsg[i][2] <= cuboidsg[j][2];
-}
-
-int LIS(int cur, int prev) {
-	if (cur == (int) cuboidsg.size())
-		return 0;
-
-	auto &ret = memory[cur][prev];
-
-	if (ret != -1)
-		return ret;
-
-	int choice1 = LIS(cur + 1, prev);		// Leave
-	int choice2 = 0;
-
-	if (prev == (int) cuboidsg.size() || less_eq(prev, cur))
-		choice2 = cuboidsg[cur][2] + LIS(cur + 1, cur);	// Take height: cuboidsg[cur][2]
-
-	return ret = max(choice1, choice2);
-}
 
 class Solution {
 public:
-	int maxHeight(vector<vector<int>> &cuboids) {
-		// Given that we can rearrange the data,
-		// then we better sort from large to small
-		// then observe, the longest decreasing sequence
-		// is actually the answer
-		// For simplicity, sort small to large and use LIS
+    static const int MAX = 100 + 1; // Make MAX a static constant
+    vector<vector<int>> cuboidsg;
+    int memory[MAX][MAX];
 
-		// Sort each cube ==> Now we can compare easily
-		for (auto &c : cuboids)	// &c important
-			sort(c.begin(), c.end());
+    bool less_eq(int i, int j) {
+        return cuboidsg[i][1] <= cuboidsg[j][1] && cuboidsg[i][2] <= cuboidsg[j][2];
+    }
 
-		// Sort all cubes ==> Answer is just LIS of the cubes
-		// Observe cube[2] now is longest side so we maximize height
-		sort(cuboids.begin(), cuboids.end());
+    int LIS(int cur, int prev) {
+        if (cur == (int)cuboidsg.size()) return 0;
 
-		cuboidsg = cuboids;
-		memset(memory, -1, sizeof(memory));
-		return LIS(0, cuboidsg.size());
-	}
+        auto &ret = memory[cur][prev];
+        if (ret != -1) return ret;
+
+        int choice1 = LIS(cur + 1, prev); // Option to leave the current cuboid
+        int choice2 = 0;
+
+        // Take the current cuboid if it fits on the previous one
+        if (prev == (int)cuboidsg.size() || less_eq(prev, cur)) {
+            choice2 = cuboidsg[cur][2] + LIS(cur + 1, cur); // Add height of the current cuboid
+        }
+
+        return ret = max(choice1, choice2);
+    }
+
+    int maxHeight(vector<vector<int>> &cuboids) {
+        if (cuboids.empty()) return 0; // Handle edge case
+
+        // Sort each cuboid's dimensions
+        for (auto &c : cuboids) sort(c.begin(), c.end());
+
+        // Sort all cuboids based on their dimensions
+        sort(cuboids.begin(), cuboids.end());
+        cuboidsg = cuboids;
+
+        memset(memory, -1, sizeof(memory)); // Initialize DP array
+        return LIS(0, cuboidsg.size());
+    }
 };
-
