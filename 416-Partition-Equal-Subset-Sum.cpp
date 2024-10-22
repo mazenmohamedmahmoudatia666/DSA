a6ef3117d66a0ea2852c9928225f1b00a0841fc7
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-10-22 11:05:07 +0300

    [416] [Time Beats: 5.01%] [Memory Beats: 46.49%] - LeetPush

--- 416-Partition-Equal-Subset-Sum.cpp
+++ 416-Partition-Equal-Subset-Sum.cpp
@@ -1,21 +1,45 @@
 class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
-        int totalSum = accumulate(nums.begin(), nums.end(), 0);
-        
-        // If the total sum is odd, it's not possible to partition it into two equal subsets
-        if (totalSum % 2 != 0) return false;
-        
-        int target = totalSum / 2;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true; // A sum of 0 is always achievable
+    vector<vector<int>> dp;
+    vector<int> arr;
 
-        for (int num : nums) {
-            for (int j = target; j >= num; j--) {
-                dp[j] = dp[j] || dp[j - num];
-            }
+    bool can(int idx, int sum) {
+        int all = accumulate(arr.begin(), arr.end(), 0);
+        int half = all / 2;
+
+        // Boundary check to avoid out-of-bounds access
+        if (sum > half) return false;
+
+        // If we reach the end of the array, check if we've hit the half sum
+        if (idx == arr.size()) {
+            return sum == half;
         }
 
-        return dp[target];
+        int &ret = dp[idx][sum];
+
+        if (ret != -1) {
+            return ret;
+        }
+
+        // Try leaving or taking the current item
+        bool leave = can(idx + 1, sum);
+        bool take = can(idx + 1, sum + arr[idx]);
+
+        return ret = (leave || take);
+    }
+
+public:
+    bool canPartition(vector<int>& nums) {
+        arr = nums;
+        int all = accumulate(nums.begin(), nums.end(), 0);
+
+        // If total sum is odd, it's impossible to partition into two equal sums
+        if (all % 2 != 0) {
+            return false;
+        }
+
+        int half = all / 2;
+        dp.resize(arr.size(), vector<int>(half + 1, -1));
+
+        return can(0, 0);
     }
 };
