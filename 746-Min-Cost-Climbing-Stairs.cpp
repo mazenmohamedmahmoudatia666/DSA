8d2a5c3cdd1670511b08ca7a7ca8cc9f69d9e6b5
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-10-28 12:24:06 +0300

    [746] [Time Beats: 100.00%] [Memory Beats: 26.99%] - LeetPush

--- 746-Min-Cost-Climbing-Stairs.cpp
+++ 746-Min-Cost-Climbing-Stairs.cpp
@@ -1,17 +1,44 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*
+
+Direct basic choices problem.
+
+At any state (idx) we can jump to idx+1 or idx+2. In both cases, we pay cost(idx)
+
+So close to Fibonacci?
+
+ */
+
+const int MAX = 1000 + 1;
+int memory[MAX];
+
+vector<int> cost;
+
+int climb(int idx) {
+	if (idx >= (int) cost.size())
+		return 0;
+
+	auto &ret = memory[idx];
+	if (ret != -1)
+		return ret;
+
+	return ret = cost[idx] + min(climb(idx + 1), climb(idx + 2));
+}
+
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n + 1); // DP array to store the minimum cost
-        
-        dp[0] = 0; // Starting point
-        dp[1] = 0; // You can start at step 0 or step 1 without any cost
-
-        // Fill the dp array
-        for (int i = 2; i <= n; ++i) {
-            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
-        }
-        
-        return dp[n]; // Minimum cost to reach the top
-    }
+	int minCostClimbingStairs(vector<int> &cost_) {
+		cost = cost_;
+		memset(memory, -1, sizeof(memory));
+		return min(climb(0), climb(1));
+	}
 };
+
