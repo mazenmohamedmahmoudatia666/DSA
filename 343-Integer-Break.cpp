56121a84de220a5d03598e4ba501a76aeb0561f3
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-10-28 11:37:16 +0300

    [343] [Time Beats: 100.00%] [Memory Beats: 93.26%] - LeetPush

--- 343-Integer-Break.cpp
+++ 343-Integer-Break.cpp
@@ -1,21 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <climits>
+#include <climits>
+using namespace std;
+
+const int MAX = 58 + 1;
+int dp[MAX];
+
 class Solution {
-   int dp[59];
-   int process(int f){
-       if (f <= 1) return 1;  // Base case
-
-       auto& ret = dp[f];
-       if (ret != -1) return ret;
-
-       ret = 0;  // Initialize to store the maximum product
-       for (int i = 1; i < f; i++) {  // Iterate up to f-1
-           ret = max(ret, max(i * (f - i), i * process(f - i)));
-       }
-       return ret;
-   }
-
 public:
-   int integerBreak(int n) {
-       memset(dp, -1, sizeof(dp));
-       return process(n);
-   }
+\tint productSum(int n) {
+\t\tif (n == 1)
+\t\t\treturn 1;
+
+\t\tauto &ret = dp[n];
+\t\tif (ret != -1)
+\t\t\treturn ret;
+
+\t\tret = n;\t// default don't divide
+\t\tfor (int i = 1; i < n; i++)
+\t\t\tret = max(ret, i * productSum(n - i));
+
+\t\treturn ret;
+\t}
+
+\tint integerBreak(int n) {
+\t\t// Special case for these 2 numbers: must k >= 2
+\t\tif (n == 2 || n == 3)
+\t\t\treturn n-1;
+
+\t\tmemset(dp, -1, sizeof(dp));
+\t\treturn productSum(n);
+\t}
 };
+
