8701268fc7099371c5eb8b74f66d9dd5bd7004e4
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-10-28 12:51:44 +0300

    [279] [Time Beats: 21.13%] [Memory Beats: 90.41%] - LeetPush

--- 279-Perfect-Squares.cpp
+++ 279-Perfect-Squares.cpp
@@ -1,51 +1,26 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
-#include <climits>
-#include <algorithm>
-#include <cassert>
-using namespace std;
-
-/*
-
-Direct loop problem
-
-Let perfectSquares(n) is our DP function
-
-What are the possible choices?
-Just loop on all perfect squares. We can simply generate them by a loop i => i * i
-	How many choices per value n? sqrt(n)
-
-So time complexity is O(n sqrt(n) )
- */
-
-const int MAX = 10000 + 1;
-int dp[MAX];
-
-//vector<int> cost;
-
-int perfectSquares(int n) {
-	if (n <= 0)
-		return 0;
-
-	auto &ret = dp[n];
-	if (ret != -1)
-		return ret;
-
-	ret = INT_MAX;
-	for (int i = 1;i * i <= n; ++i) 	// a smart condition is i * i <= n
-		ret = min(ret, 1 + perfectSquares(n - i * i));
-	
-	return ret;
-}
-
 class Solution {
+    int dp[10001];
+     int perfet(int m){
+        int perfect_square = sqrt(m);
+        if (m == 0) {
+            return 0;
+        }
+
+
+        auto&ret = dp[m];
+        if(ret != -1)
+        return ret;
+
+          ret = INT_MAX;
+         for(int i = 1; i <= sqrt(m); i++){
+            ret = min(ret , 1 + perfet(m - i*i));
+         } 
+
+        return ret;
+     }
 public:
-	int numSquares(int n) {
-		memset(dp, -1, sizeof(dp));
-		return perfectSquares(n);
-	}
+    int numSquares(int n) {
+        memset(dp , -1 , sizeof(dp));
+        return perfet(n);
+    }
 };
-
-
-
