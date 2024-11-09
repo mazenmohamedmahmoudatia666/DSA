b37ac54d6cd9fa6a016deed669a154145f0a296d
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-11-09 13:34:17 +0200

    [1105] [Time Beats: 100.00%] [Memory Beats: 97.84%] - LeetPush

--- 1105-Filling-Bookcase-Shelves.cpp
+++ 1105-Filling-Bookcase-Shelves.cpp
@@ -1,30 +1,24 @@
 class Solution {
-    vector<int> dp; // 1D DP array to store minimum height for books from start up to index `i`
-    
-    int process(int i, vector<vector<int>>& books, int shelfWidth) {
-        if (i >= books.size()) return 0; // Base case: no more books to place
-        
-        auto&ret = dp[i];
-        if ( ret!= -1) return ret; // If already computed, return the result
-        
-        int width = 0, height = 0, minHeight = INT_MAX;
-
-        // Try placing books on the current shelf up to the width limit
-        for (int j = i; j < books.size(); j++) {
-            width += books[j][0]; // Accumulate book width
-
-            if (width > shelfWidth) break; // Exceeds width, stop adding books
-
-            height = max(height, books[j][1]); // Track the maximum height on the current shelf
-            minHeight = min(minHeight, height + process(j + 1, books, shelfWidth)); // Recursive step
-        }
-
-        return ret = minHeight; // Store the result in dp array
-    }
+  
     
 public:
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
-        dp = vector<int>(books.size(), -1); // Initialize dp array with -1 for memoization
-        return process(0, books, shelfWidth); // Start from the first book
+        int n = books.size();
+        vector<int> dp(n + 1 , INT_MAX);
+
+        dp[0] = 0;
+   
+         for(int i = 1; i <= n; i++){
+            int total_width = 0 , max_height = 0;
+            for(int j = i- 1; j >= 0; j--){
+            total_width += books[j][0];
+            if(total_width > shelfWidth) break;
+            max_height = max(max_height , books[j][1]);
+            
+           dp[i] = min(dp[i] , dp[j] + max_height);            
+         }
+         }
+
+        return dp[n];
     }
 };
