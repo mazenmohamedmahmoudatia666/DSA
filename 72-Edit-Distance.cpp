0ac42f9add32d1ffd019e0709a334647e333ec81
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-11-09 12:37:36 +0200

    [72] [Time Beats: 67.74%] [Memory Beats: 63.87%] - LeetPush

--- 72-Edit-Distance.cpp
+++ 72-Edit-Distance.cpp
@@ -1,36 +1,38 @@
 class Solution {
-private:
-    string s, m;
-    int dp[501][501];
-
-    int process(int i, int j) {
-        if (i == s.size()) return m.size() - j; // Insert remaining characters of m
-        if (j == m.size()) return s.size() - i; // Delete remaining characters of s
-
-        auto &ret = dp[i][j];
-        if (ret != -1) return ret;
-
-        int insert = 0, remove = 0, match = 0;
-        if (s[i] == m[j]) {
-            match = process(i + 1, j + 1); // Characters match, no edit needed
-        } else {
-            match = 1 + process(i + 1, j + 1); // Substitute
-        }
-
-        remove = 1 + process(i + 1, j); // Delete s[i]
-        insert = 1 + process(i, j + 1); // Insert m[j] into s
-
-        ret = min({match, remove, insert});
-
-        return ret;
-    }
-
 public:
     int minDistance(string word1, string word2) {
-        s = word1;
-        m = word2;
-        memset(dp, -1, sizeof(dp));
+        int n = word1.size(), m = word2.size();
+        
+        // Create DP table
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-        return process(0, 0);
+        // Base cases: dp[i][0] means transforming word1[0..i-1] to an empty string (deletions)
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;  // i deletions required to convert word1[0..i-1] to empty
+        }
+        
+        // Base cases: dp[0][j] means transforming an empty string to word2[0..j-1] (insertions)
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;  // j insertions required to convert empty to word2[0..j-1]
+        }
+
+        // Fill the DP table using the recurrence relation
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    // No operation needed if the characters are the same
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // Otherwise, consider insert, delete, or substitute
+                    dp[i][j] = min({dp[i - 1][j] + 1,    // Deletion
+                                     dp[i][j - 1] + 1,    // Insertion
+                                     dp[i - 1][j - 1] + 1 // Substitution
+                    });
+                }
+            }
+        }
+
+        // The bottom-right corner of the DP table will contain the minimum edit distance
+        return dp[n][m];
     }
 };
